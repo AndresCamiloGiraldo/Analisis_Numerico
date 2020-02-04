af71debad6b159dc1842cc59bf134d060f9b93b8
Andres Gil <andrew.2904@outlook.com>
2020-02-04 15:26:02 -0500

    Create Problema_6.R

---
+++ Talleres/Taller_1/Problemas_Taller_1/Problema_6.R
@@ -0,0 +1,16 @@
+#--------------------------------
+# TALLER #1
+# PROBLEMA #6
+#--------------------------------
+# Integrantes
+# - Andres Camilo Giraldo Gil
+# - Erika Alejandra Gonzalez
+# - Leonel Steven Londono
+#--------------------------------
+# Analisis Numerico
+#--------------------------------
+
+
+
+
+#--------------------------------
