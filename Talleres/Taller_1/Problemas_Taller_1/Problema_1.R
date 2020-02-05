acf52bf7788ecee2c01d54a14e864a1e1f1b5e1d
Andres Gil <andrew.2904@outlook.com>
2020-02-04 20:10:46 -0500

    Create Problema_1.R

---
+++ Talleres/Taller_1/Problemas_Taller_1/Problema_1.R
@@ -0,0 +1,29 @@
+#--------------------------------
+# TALLER #1
+# PROBLEMA #1
+#--------------------------------
+# Integrantes
+# - Andres Camilo Giraldo Gil
+# - Erika Alejandra Gonzalez
+# - Leonel Steven Londono
+#--------------------------------
+# Analisis Numerico
+#--------------------------------
+
+errorTruncamiento  =  function ( maxDigitos , numero ) {
+  contador  =  0
+  aux  =  numero
+  while ( aux > 1 )
+  {
+    aux  =  aux / 10
+    
+    contador  =  contador + 1
+  }
+  auxTruncado  = trunc ( aux * 10 ^ maxDigitos ) / 10 ^ maxDigitos
+  solucion  = ( aux  -  auxTruncado ) * 10 ^ ( contador - 1 )
+  imprimir ( solucion )
+  
+}
+errorTruncamiento ( 4 , 536.78 )
+
+#--------------------------------
